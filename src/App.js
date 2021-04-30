5ac71bf018b391e4f7b75cdc8ef11c75c32624ab
llennairek <llennairek@gmail.com>
2021-04-30 17:23:04 +0200

    added dark theme switch - have to resolve the bg color of body tag

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TodoList from "./components/TodoList";
+import { useState } from "react";
 
 // IMPORTING FONT AWESOME ICONS
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -9,9 +10,11 @@ import { faListAlt, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 library.add(faListAlt, faTrashAlt);
 
 function App() {
+  const [theme, setTheme] = useState("theme-dark");
+
   return (
-    <div>
-      <Header />
+    <div className={`app ${theme}`}>
+      <Header theme={theme} setTheme={setTheme} />
       <TodoList />
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Header() {
+function Header({ theme, setTheme }) {
+  const handleClick = () => {
+    if (theme === "theme-light") {
+      setTheme("theme-dark");
+    } else {
+      setTheme("theme-light");
+    }
+  };
   return (
     <header>
       <FontAwesomeIcon icon="list-alt" />
       <h1>Todo List</h1>
+      <div
+        className={theme === "theme-light" ? "theme" : "theme isActive"}
+        onClick={handleClick}
+      ></div>
     </header>
   );
 }
